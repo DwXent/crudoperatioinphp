f034f0575350d092cb9170ec32553237ad5c276e
Raman <ramanthakur471@gmail.com>
2022-01-28 11:11:35 +0530

    Initial commit

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+include 'connect.php';
+if(isset($_GET['deleteid'])){
+    $id=$_GET['deleteid'];
+
+    $sql="delete from `crudon` where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+        echo "Deleted successfully";
+    }else{
+        die(mysqli_error($con));
+    }
+}
+
+?>
